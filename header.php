8ed8ec037763133cebef367490f6b92b0fab96a1
Mireliana Zyra <mirelianazyra@Mirelianas-MacBook-Pro.local>
2024-02-23 17:01:16 -0500

    update: adding analytics tracking to theme

--- header.php
+++ header.php
@@ -16,6 +16,15 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-SHFKHJKB0T"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-SHFKHJKB0T');
+	</script>
 
 	<?php wp_head(); ?>
 </head>
