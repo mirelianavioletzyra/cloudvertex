a559777f312b0ae67420b5ed6e2853e639122ac6
Mireliana Zyra <mirelianazyra@Mirelianas-MacBook-Pro.local>
2024-03-06 17:43:39 -0500

    mega menu compatability

--- header.php
+++ header.php
@@ -66,7 +66,7 @@
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'tailwind'); ?></button>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"></button>
 					<?php
 					wp_nav_menu(
 						array(
