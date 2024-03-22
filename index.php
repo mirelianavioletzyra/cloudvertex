d17d35a904153bdf536413ed8c672637d63a3e78
Mireliana Zyra <mirelianazyra@Mirelianas-MacBook-Pro.local>
2024-03-22 15:51:30 -0400

    feat: blog styles base and grid system revisement

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ get_header();
 	if (have_posts()) :
 
 		if (is_home() && !is_front_page()) :
-	?>
+		?>
 			<header>
 				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 			</header>
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,13 +10,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<!-- <?php // the_title( '<h1 class="entry-title text-magenta text-2xl">', '</h1>' ); ?> -->
-	</header><!-- .entry-header -->
+	<?php if ( ! is_front_page() ) : ?>
+		<header class="entry-header grid grid-cols-12 mt-24">
+			<?php the_title( '<h1 class="entry-title text-magenta text-2xl col-span-8 col-start-2 md:col-start-3">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+	<?php endif; ?>
 
 	<?php tailwind_post_thumbnail(); ?>
 
-	<div class="entry-content pt-20">
+	<div class="entry-content grid grid-cols-12">
 		<?php
 		the_content();
 
