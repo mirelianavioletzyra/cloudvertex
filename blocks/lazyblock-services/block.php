97e6f7c894a65a1e774a1a2b5116bfaec14d3e8e
Mireliana Zyra <mirelianazyra@Mirelianas-MacBook-Pro.local>
2024-04-22 15:29:59 -0400

    fix: replacing grid with flexbox

--- blocks/lazyblock-services/block.php
+++ blocks/lazyblock-services/block.php
@@ -9,17 +9,17 @@
  */
 
 ?>
-<div class="services-block wp-container-core-columns-layout-1 grid">
-    <div class="flex flex-col justify-center text-center">
+<div class="services-block wp-container-core-columns-layout-1 flex justify-center flex-wrap">
+    <div class="flex flex-col justify-center text-center w-full">
         <h2 class="text-center"><?php echo $attributes['title']; ?></h2>
 
         <h3><?php echo $attributes['sub_heading']; ?></h3>
     </div>
 
     <?php foreach ($attributes['service'] as $inner) : ?>
-        <div class="grid grid-cols-1 justify-center text-center services">
+        <div class="max-w-[3.6rem] md:max-w-xs justify-center text-center services">
             <img src="<?php echo $inner['icon']['url']; ?>">
-            <p class="text-xs md:text-md xl:text-lg">
+            <p class="text-[0.6rem] md:text-md xl:text-lg">
                 <?php echo $inner['service_name']; ?>
             </p>
         </div>
